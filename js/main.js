7e0fcdd84ead43b6bcb94353aa0cb3432e14556f
Yordan Iliev <shinzuiuindo@mail.bg>
2015-02-13 00:16:38 +0200

    13-02-2015

--- js/main.js
+++ js/main.js
@@ -1,103 +1,60 @@
 $(document).ready(function () {
 
+//-----------
+//  Означава, че всичко, което е заредено в HTML-а е  -> rendered, CSS - loaded, JS - loaded
+//-----------
 
-      $('#login-button').on('click', function(e){
-          e.preventDefault();
-          checkLogin();
-
-      })
-
-
-    $('#register-button').on('click', function(e){
-        e.preventDefault();
-        window.location.href = 'register.html';
-    })
-
-    $('#confirm-register').on('click', function(e){
-        e.preventDefault();
-        $('#myModal').modal('show')
-    })
-
-    $('#ok-register').on('click', function(e){
-        e.preventDefault();
-        window.location.href = 'index.html';
-    })
-
-
-    loadCategories('GET','webservices/categories.json',"");
-
-
+    init(function(){
+      //  renderSmallColumns();
+    });
 
 
 
 });
+var rowCount = 5;
+var columnCount = 5;
 
+var boxSize  = 50;
 
-function checkLogin(){
+var rowWidthPx = (columnCount * 2 + 1)* boxSize;
+var rowHeight = boxSize;
 
-    var loginData = {};
-    loginData.username = $("#user-name").val();
-    loginData.password = $("#password").val() ;
-
-    $.ajax({
-        type: "POST",
-        url: "webservices/login.json",
-        data: loginData,
-        dataType: "json",
-        success: function (response) {
-
-            if(response.status.login == "success"){
-                console.log("success");
-                window.location.href = 'home.html';
-            }
-
-        },
-
-        error: function (data) {
-
-
-        }
-    }).always(function () {
+var smallColumn = "<div class='red-column small-column'></div>";
+var renderedSmallColumns = false;
+var smallColumnSize = 300;
+var names = ["Първа колонка", "Втора колонка", "Трета колонка", "Четвърта колонка"];
 
 
 
-        });
-}
 
-function loadCategories(type,service,data){
+function init(callback){
+    Maze.generate("maze-container",rowCount,columnCount,1);
+    $('#maze-container').find('.r').css({'width': rowWidthPx, 'height': rowHeight });
+    $('#maze-container').find('.b').css({'width': boxSize, 'height': boxSize });
+    $('#maze-container').find('.w').css({'width': boxSize, 'height': boxSize });
 
-
-    $.ajax({
-        type: type,
-        url: service,
-        data: data,
-        dataType: "json",
-        success: function (response) {
-
-            $.each(response.categories, function(index,category){
-                renderCategory(category);
-                console.log(category)
-                console.log(category.name)
-                console.log(category.image)
-            })
-
-        },
-
-        error: function (data) {
-
-
-        }
-    }).always(function () {
-
-
-
-        });
-
+    console.log('Initialized...', 'Small Column Size: ', smallColumnSize);
+    if(callback){
+        callback();
+    }
 }
 
 
-function renderCategory(category){
+function renderSmallColumns(){
 
-    $("#categories-container").append('<div class="row"><div class="col-lg-4"><img src="'+category.image+'" alt="'+category.name+'" class="img-thumbnail"></div><div class="col-lg-8"><div class="panel panel-default"><div class="panel-heading"><h3 class="panel-title">'+category.name+'</h3></div><div class="panel-body">'+category.description+'</div></div></div></div>');
-    $("#menu-left").append('<li class="list-group-item item-container">'+ category.name+'</li>');
+    var $container = $('.main-container')
+
+    // Underscore each -> борави с колекции и масиви
+    _.each(names, function(valueInNames, index){
+        $container.append(smallColumn);
+        console.log(valueInNames, index)
+    })
+
+    // jQuery each -> борави с преселектирани HTML елементи
+    $container.find('.small-column').each( function (index, smallColumn){
+        console.log( index, smallColumn);
+        $(smallColumn).html(names[index])
+    })
+
+    renderedSmallColumns = true;
 }
